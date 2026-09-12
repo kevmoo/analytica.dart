0c30074a6bb18da074874178954dad3b4e9d4bc2
Kevin Moore <kevmoo@google.com>
2026-09-12 22:51:37 +0000

    test(e2e): simplify probe again to verify resolved SHA

--- packages/cognitive_complexity/lib/src/e2e_probe.dart
+++ packages/cognitive_complexity/lib/src/e2e_probe.dart
@@ -1,34 +1,5 @@
 /// Throwaway probe. Deleted before merge.
 library;
 
-/// Deliberately convoluted; scores well above a threshold of 15.
-int probe(int x) {
-  if (x > 0) {
-    for (var i = 0; i < x; i++) {
-      if (i % 2 == 0) {
-        x++;
-      } else {
-        x--;
-      }
-    }
-    if (x > 1) {
-      for (var i = 0; i < x; i++) {
-        if (i % 2 == 0) {
-          x++;
-        } else {
-          x--;
-        }
-      }
-      if (x > 2) {
-        for (var i = 0; i < x; i++) {
-          if (i % 2 == 0) {
-            x++;
-          } else {
-            x--;
-          }
-        }
-      }
-    }
-  }
-  return x;
-}
+/// Simplified: scores 1, well under the threshold of 15.
+int probe(int x) => x + 1;
