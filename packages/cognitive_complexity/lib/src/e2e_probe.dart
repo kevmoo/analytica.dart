b1005a24e4ac8a485b6ce0c69218ca32f603b3b4
Kevin Moore <kevmoo@google.com>
2026-09-12 22:49:51 +0000

    test(e2e): re-introduce violation to verify head SHA

--- packages/cognitive_complexity/lib/src/e2e_probe.dart
+++ packages/cognitive_complexity/lib/src/e2e_probe.dart
@@ -1,6 +1,34 @@
-/// Throwaway probe used during an end-to-end dry run of the quiet-comment
-/// action. Deleted before merge.
+/// Throwaway probe. Deleted before merge.
 library;
 
-/// Simplified: scores 1, well under the threshold of 15.
-int probe(int x) => x + 1;
+/// Deliberately convoluted; scores well above a threshold of 15.
+int probe(int x) {
+  if (x > 0) {
+    for (var i = 0; i < x; i++) {
+      if (i % 2 == 0) {
+        x++;
+      } else {
+        x--;
+      }
+    }
+    if (x > 1) {
+      for (var i = 0; i < x; i++) {
+        if (i % 2 == 0) {
+          x++;
+        } else {
+          x--;
+        }
+      }
+      if (x > 2) {
+        for (var i = 0; i < x; i++) {
+          if (i % 2 == 0) {
+            x++;
+          } else {
+            x--;
+          }
+        }
+      }
+    }
+  }
+  return x;
+}
