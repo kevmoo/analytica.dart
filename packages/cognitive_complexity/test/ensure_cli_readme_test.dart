05d2ee2be7f03d007f60024fd0d454817315e844
Kevin Moore <kevmoo@google.com>
2026-09-17 22:41:53 +0000

    test(cognitive_complexity): increase ensure_cli_readme_test timeout for 3 executables

--- packages/cognitive_complexity/test/ensure_cli_readme_test.dart
+++ packages/cognitive_complexity/test/ensure_cli_readme_test.dart
@@ -2,5 +2,9 @@ import 'package:cli_readme/cli_readme.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_cli_readme', expectReadmeHelpClean);
+  test(
+    'ensure_cli_readme',
+    expectReadmeHelpClean,
+    timeout: const Timeout(Duration(minutes: 2)),
+  );
 }
